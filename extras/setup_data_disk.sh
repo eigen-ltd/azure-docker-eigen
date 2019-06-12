59a0c64dcbc2b517380d15db69ac45ec347385b1
Paul Field <paul.william.field@gmail.com>
2019-06-12 16:36:33 +0100

    create docker mounts lvm

--- extras/setup_data_disk.sh
+++ extras/setup_data_disk.sh
@@ -18,23 +18,24 @@ pvcreate /dev/sdc
 # Create a volume group. In this example we are calling the volume group data-vg01:
 vgcreate dockervg /dev/sdc
 
-dockervg
 # Create the logical volume logical volume called data-lv01 
 #lvcreate --extents 100%FREE  --name data-lv01 data-vg01
-lvcreate -n docker--storage -L 20G dockervg
-lvcreate -n docker--workspaces -L 10G dockervg
+lvcreate --size 20GB --name docker-storage dockervg
+lvcreate --size 10GB --name docker-workspaces dockervg
+lvscan
 
 # Format the logical volume
-mkfs -t ext4 /dev/data-vg01/dockervg
+mkfs.ext4 /dev/dockervg/docker-storage
+mkfs.ext4 /dev/dockervg/docker-workspaces
 
 # Create the desired mount point for your new file system
+mkdir /var/lib/docker
 mkdir /var/lib/docker-workspaces
+
 # mount the disk
-# mount /dev/sdc1 /data/
-mount /dev/dockervg/docker--storage /var/lib/docker
-mount /dev/dockervg/docker--workspaces /var/lib/docker-workspaces
+mount /dev/dockervg/docker-storage /var/lib/docker
+mount /dev/dockervg/docker-workspaces /var/lib/docker-workspaces
 
 # add mount to /etc/fstab to persist across reboots
-#echo "/dev/data-vg01/data-lv01    /data/    ext4    defaults 0 2" >> /etc/fstab
-echo "/dev/dockervg/docker--storage    /var/lib/docker    ext4    defaults,nofail 1 2" >> /etc/fstab
-echo "/dev/dockervg/docker--workspaces     /var/lib/docker-workspaces    ext4    defaults,nofail 1 2" >> /etc/fstab
+echo "/dev/mapper/dockervg-docker--storage /var/lib/docker    ext4    defaults,nofail        1 2" >> /etc/fstab
+echo "/dev/mapper/dockervg-docker--workspaces /var/lib/docker-workspaces	ext4    defaults,nofail        1 2" >> /etc/fstab
